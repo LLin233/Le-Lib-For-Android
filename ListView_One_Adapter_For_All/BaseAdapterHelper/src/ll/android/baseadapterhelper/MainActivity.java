bf9433bb952cf947e9d221e28e7dc59931ca0a03
leonard-lin <linle.job@gmail.com>
2015-01-28 14:11:36 -0800

    CommonAdapter for ListView
    Adapt to a variety of layouts in ListView with little changes to adapter
    and viewHolder

---
+++ ListView_One_Adapter_For_All/BaseAdapterHelper/src/ll/android/baseadapterhelper/MainActivity.java
@@ -0,0 +1,49 @@
+package ll.android.baseadapterhelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private ListView mListView;
+	private List<String> mDatas = new ArrayList<String>(Arrays.asList("List",
+			"View", "Common"));
+//	private MyAdapter mAdapter;
+	private CommonAdapter<String> mAdapter2;
+
+	// @Override
+	// protected void onCreate(Bundle savedInstanceState)
+	// {
+	// super.onCreate(savedInstanceState);
+	// setContentView(R.layout.main);
+	// mListView = (ListView) findViewById(R.id.id_lv_main);
+	// mListView.setAdapter(mAdapter = new MyAdapter(this, mDatas));
+	//
+	// }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mListView = (ListView) findViewById(R.id.id_lv_main);
+
+		// …Ë÷√  ≈‰∆˜
+		mListView.setAdapter(mAdapter2 = new CommonAdapter<String>(
+				getApplicationContext(), mDatas, R.layout.list_item) {
+			@Override
+			public void convert(ViewHolder viewHolder, String item) {
+				TextView view = viewHolder.getView(R.id.id_tv_title);
+				view.setText(item);
+			}
+
+		});
+
+	}
+}
